b735bc00b1280eb5f5431fa7d13d5ea6b00702d1
Clique33 <gabriegcc@gmail.com>
2026-06-10 12:22:23 -0300

    added shooting sfx

--- gnoma-x-aliens/Entities/shooting/gun.gd
+++ gnoma-x-aliens/Entities/shooting/gun.gd
@@ -13,6 +13,9 @@ var _projectile_pool : Array[Projectile] = []
 var _available_projectiles: Array[int]   = []
 
 
+@onready var shoot_sfx: AudioStreamPlayer2D = $ShootSfx
+
+
 func _ready() -> void:
 	var bullets : Node2D = get_tree().current_scene.get_node_or_null("Projectiles")
 	if not bullets:
@@ -38,6 +41,7 @@ func shoot_to(direction : Vector2) -> void:
 	projectile.scale = projectile_scale
 	projectile.is_from_player = gun_is_from_player
 	projectile.enabled = true
+	shoot_sfx.play()
 
 
 func shoot_at(node : Node2D) -> void:
