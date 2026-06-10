e62a4eda42ab1b6127b6abec2ee4f919ccc93c7b
Clique33 <gabriegcc@gmail.com>
2026-06-10 12:22:44 -0300

    added shooting and walking animations

--- gnoma-x-aliens/systems/components/attack_component.gd
+++ gnoma-x-aliens/systems/components/attack_component.gd
@@ -26,4 +26,5 @@ func _physics_process(_delta: float) -> void:
 			gun.global_position = right_point.global_position
 			direction = Vector2.RIGHT
 		gun.shoot_to(direction)
+		parent.base_sprite.play("shoot")
 		cooldown.start(shooting_cooldown_time)
