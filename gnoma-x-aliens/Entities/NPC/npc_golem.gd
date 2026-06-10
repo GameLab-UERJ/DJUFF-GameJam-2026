d7d7f0c9be7296a776da2878c7ba38b818ab0dc1
JPLMB <145187137+JPLMB@users.noreply.github.com>
2026-06-10 20:42:59 -0300

    refatoração dos golems + uso do dialogue manager

--- gnoma-x-aliens/Entities/NPC/npc_golem.gd
+++ gnoma-x-aliens/Entities/NPC/npc_golem.gd
@@ -1,30 +1,22 @@
 extends StaticBody2D
 
-var can_interact: bool = false
-var dialogue_index: int = 0
+@export var dialogue_resource: DialogueResource
+@export var dialogue_title: String
 
-@export var dialogue_list: Array[String] = ["hi", "hello", "ola"]
+var can_interact := false
 
-@onready var canvas: CanvasLayer = $CanvasLayer
-@onready var dialogue_label: Label = $CanvasLayer/Dialogue_Label
 @onready var interact_area: Area2D = $InteractArea
 
 func _ready() -> void:
 	interact_area.body_entered.connect(_on_body_entered)
 	interact_area.body_exited.connect(_on_body_exited)
-	canvas.visible = false
 
-func _input(event: InputEvent) -> void:
+func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("interact") and can_interact:
-		if dialogue_index < dialogue_list.size():
-			canvas.visible = true
-			get_tree().paused = true
-			dialogue_label.text = dialogue_list[dialogue_index]
-			dialogue_index += 1
-		else:
-			canvas.visible = false
-			get_tree().paused = false
-			dialogue_index = 0
+		DialogueManager.show_dialogue_balloon(
+			dialogue_resource,
+			dialogue_title
+		)
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
@@ -33,6 +25,3 @@ func _on_body_entered(body: Node2D) -> void:
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		can_interact = false
-		canvas.visible = false
-		get_tree().paused = false
-		dialogue_index = 0
